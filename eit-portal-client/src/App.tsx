419940eb1bbc1a09c836bbe4be245be04a7320f9
Stephen Gibbon <steve64g@gmail.com>
2021-07-22 21:07:47 +1000

    tweaks

--- eit-portal-client/src/App.tsx
+++ eit-portal-client/src/App.tsx
@@ -30,32 +30,10 @@ function App() {
             count is: {count}
           </button>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
       </header>
       <main>
         {data?.map((x) => (
-          <div key={x.date}>
+          <div key={x.date} className="App-list">
             <div>{x.date}</div>
             <div>{x.temperatureC}</div>
           </div>
